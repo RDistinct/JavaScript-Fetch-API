7322e781f388ca8a11582af502e7fe86a26e27d1
ReubenDev <reubenkiarie8m@gmail.com>
2023-07-17 19:45:28 +0300

    add error handling and asynchronous function

--- Pokemon API/main.js
+++ Pokemon API/main.js
@@ -1,40 +1,72 @@
 document.getElementById('search').addEventListener('click', getPokemonDetails);
 
-// mAKE the first letter of the pokemon name capital
+// make the first letter of the pokemon name capital
 function makeFirstLetterCapital(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-// APIs dont understand uppercase letters so the input should be in lowercase.Use a function to convert input to lowercase.
+// APIs don't understand uppercase letters so the input should be in lowercase.Use a function to convert input to lowercase.
 function setToLowercase(string){
     return string.toLowerCase();
 }
+
 // Fetch data from pokemon API
-function getPokemonDetails(e){
+/* function getPokemonDetails(e){
     const pokemonName = document.getElementById('pokemonName').value;
+    console.log(pokemonName);
     const nameValue = setToLowercase(pokemonName);
 
-    // here we are using backticks to enable us to write JS code on the api URL.If you use qutes, it wont work
+    // here we are using backticks to enable us to write JS code on the api URL.If you use quotes, it wont work
     fetch(`https://pokeapi.co/api/v2/pokemon/${nameValue}`)
-        .then(response => response.json())
-        .then(data => {
-            document.querySelector('.pokemonBox').innerHTML = 
+    .then(response => {
+        //handle error
+        console.log(response);
+        if(!response.ok){
+            throw new Error(`Sorry. Error ${response.status}`)
+        }else
+           return response.json()
+        })
+    .then((data) => {
+        console.log(data);
+         document.querySelector('.pokemonBox').innerHTML = 
                 ` <div>
                     <img src="${data.sprites.other["official-artwork"]["front_default"]}" 
                     alt="${makeFirstLetterCapital(data.name)}">
                  </div>
 
-                <div class="pokemonInfo">
+                 <div class="pokemonInfo">
                     <h1>${makeFirstLetterCapital(data.name)}</h1>
-                    <span>Weight description: ${data.weight} </span>
-                    <span>Height: ${data.height}<span/>
-                </div>
-                
-                <div>
-                    <p>Ability: ${data.abilities[0].ability.name}<p/>
-                <div/>`
+                    <p>Weight description: ${data.weight} </p>
+                </div>` 
         })
-        .catch(err => console.log("Error! Pokemon not found",err))
+    .catch(err => console.log(`Error! Pokemon not found,${err}`))
 
         e.preventDefault();
-}
+} */
 
+//async version getPokemonDetails
+async function getPokemonDetails(e){
+    const pokemonName = document.getElementById('pokemonName').value;
+    console.log(pokemonName);
+    const nameValue = setToLowercase(pokemonName);
+
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameValue}`)
+
+        if(!response.ok){
+            throw new Error(`response.status`)
+        }
+
+        const data = await response.json();
+        document.querySelector('.pokemonBox').innerHTML = 
+                ` <div>
+                    <img src="${data.sprites.other["official-artwork"]["front_default"]}" 
+                    alt="${makeFirstLetterCapital(data.name)}">
+                 </div>
+
+                 <div class="pokemonInfo">
+                    <h1>${makeFirstLetterCapital(data.name)}</h1>
+                    <p>Weight description: ${data.weight} </p>
+                </div>`
+
+    } catch(error){}
+}
